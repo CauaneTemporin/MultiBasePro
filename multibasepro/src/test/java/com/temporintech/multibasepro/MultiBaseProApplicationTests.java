d027ca6719c32a3d6975e9c14adaa4a9c22ae9f1
Cauane Temporin <temporin.cauane@gmail.com>
2024-03-10 22:59:42 -0300

    Criação do projeto MultiBasePro

---
+++ multibasepro/src/test/java/com/temporintech/multibasepro/MultiBaseProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temporintech.multibasepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBaseProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
