bd5b4ae4ba69989c65d16ceccd1fb2f9474d4047
Cauane Temporin <temporin.cauane@gmail.com>
2024-03-13 12:00:21 -0300

    Adicionando a implementação do LivroServiceImpl

---
+++ multibasepro/src/main/java/com/temporintech/multibasepro/service/impl/LivroServiceImpl.java
@@ -0,0 +1,28 @@
+package com.temporintech.multibasepro.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.temporintech.multibasepro.model.app.Livro;
+import com.temporintech.multibasepro.repository.LivroRepository;
+import com.temporintech.multibasepro.service.LivroService;
+
+@Service
+public class LivroServiceImpl implements LivroService{
+
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	@Override
+	public List<Livro> obterLivros() {
+		return this.livroRepository.findAll();
+	}
+
+	@Override
+	public Livro criar(Livro livro) {
+		return null;
+	}
+
+}
