70e1fd3c5719a7f6aaadb08fae93286a84f1912f
CauaneTemporin <temporin.cauane@gmail.com>
2024-03-16 08:04:27 -0300

    EntityManager para o banco APP

--- multibasepro/src/main/java/com/temporintech/multibasepro/config/AppDbConfig.java
+++ multibasepro/src/main/java/com/temporintech/multibasepro/config/AppDbConfig.java
@@ -2,20 +2,34 @@ package com.temporintech.multibasepro.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 public class AppDbConfig {
-
+	
 	@Bean
-            @Primary
+	@Primary
 	@ConfigurationProperties(prefix = "app.datasource")
 	public DataSource appDataSource() {
 		return DataSourceBuilder.create().build();
 	}
 
+	@Bean
+	@Primary
+	public LocalContainerEntityManagerFactoryBean appEntityManager(
+			EntityManagerFactoryBuilder builder,
+			@Qualifier("appDataSource") DataSource dataSource) {
+		return builder
+				.dataSource(dataSource)
+				.packages("com.temporintech.multibasepro.model.app")
+				.build();
+	}
+	
 }
