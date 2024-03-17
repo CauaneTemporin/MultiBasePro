0e3a618d360b90311ca95a56c8b7a5051e2cc0e1
CauaneTemporin <temporin.cauane@gmail.com>
2024-03-17 18:47:14 -0300

    EntityManager para o banco AUTH

--- multibasepro/src/main/java/com/temporintech/multibasepro/config/AuthDbConfig.java
+++ multibasepro/src/main/java/com/temporintech/multibasepro/config/AuthDbConfig.java
@@ -2,10 +2,13 @@ package com.temporintech.multibasepro.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 public class AuthDbConfig {
@@ -14,5 +17,15 @@ public class AuthDbConfig {
 	@ConfigurationProperties(prefix = "auth.datasource")
 	public DataSource authDataSource() {
 		return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean authEntityManager(
+			EntityManagerFactoryBuilder builder,
+			@Qualifier("authDataSource") DataSource dataSource) {
+		return builder
+				.dataSource(dataSource)
+				.packages("com.temporintech.multibasepro.model.auth")
+				.build();
 	}
 }
